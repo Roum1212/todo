863b94e282660c9c453368703c198af09909e57e
tarasteresenkov <taras131014@gmail.com>
2025-05-23 16:44:50 +0300

    GO-8: code cleanup

--- internal/domain/model/reminder-id/model_test.go
+++ internal/domain/model/reminder-id/model_test.go
@@ -4,18 +4,24 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint:depguard // OK.
 )
 
 func TestGenerateReminderID(t *testing.T) {
+	t.Parallel()
+
 	id1 := GenerateReminderID()
+
 	time.Sleep(1 * time.Second)
+
 	id2 := GenerateReminderID()
 
 	require.NotEqual(t, id1, id2, "Reminder ID should not be the same")
 }
 
 func TestGenerateReminderIDValid(t *testing.T) {
+	t.Parallel()
+
 	text, err := NewReminderID("1234567890")
 
 	require.NoError(t, err)
@@ -23,6 +29,8 @@ func TestGenerateReminderIDValid(t *testing.T) {
 }
 
 func TestGenerateReminderIDInvalid(t *testing.T) {
+	t.Parallel()
+
 	str := "abc"
 	_, err := NewReminderID(str)
 
