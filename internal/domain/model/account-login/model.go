2cfea3745456272bcf593eb3b43a3fa2e6ef2a1a
tarasteresenkov <taras131014@gmail.com>
2025-06-18 12:25:23 +0300

    main: delete account model

--- internal/domain/model/account-login/model.go
+++
@@ -1,7 +0,0 @@
-package account_login_model
-
-type AccountLogin string
-
-func NewAccountLogin(login string) AccountLogin {
-	return AccountLogin(login)
-}
--- internal/domain/model/account-password/model.go
+++
@@ -1,7 +0,0 @@
-package account_password_model
-
-type AccountPassword string
-
-func NewAccountPassword(password string) AccountPassword {
-	return AccountPassword(password)
-}
